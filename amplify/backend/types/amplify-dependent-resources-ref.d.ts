989ce06a5b850fb34e2873eb8a1502db4ca44889
sairaghav <4383992+sairaghav@users.noreply.github.com>
2022-11-02 16:39:21 +0100

    Revert "Run prettier"
    This reverts commit 90bdab4a5d629901cf1baee1bc656079770b2b01.
    
    Revert plop_templates prettier

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,17 +1,17 @@
 export type AmplifyDependentResourcesAttributes = {
-  function: {
-    getPaperInfo: {
-      Name: "string";
-      Arn: "string";
-      Region: "string";
-      LambdaExecutionRole: "string";
-    };
-  };
-  api: {
-    backend: {
-      RootUrl: "string";
-      ApiName: "string";
-      ApiId: "string";
-    };
-  };
-};
+    "function": {
+        "getPaperInfo": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "backend": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
