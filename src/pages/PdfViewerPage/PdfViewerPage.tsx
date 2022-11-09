ca77445b43df3a8371af7770375cb563558789bf
sairaghav <4383992+sairaghav@users.noreply.github.com>
2022-11-09 15:37:57 +0100

    init pdfViewer

--- src/pages/PdfViewerPage/PdfViewerPage.tsx
+++ src/pages/PdfViewerPage/PdfViewerPage.tsx
@@ -1,7 +1,15 @@
-import React, { useMemo, FC } from "react";
+import React, {
+  useMemo,
+  FC,
+  useEffect,
+  useState,
+  useRef,
+  useCallback,
+} from "react";
 import cs from "classnames";
-
 import { PdfViewerPageProps } from "./PdfViewerPage.types";
+import * as pdfjs from "pdfjs-dist/legacy/build/pdf";
+//import * as pdfjsWorker from "pdfjs-dist/build/pdf.worker.entry";
 
 const PdfViewerPage: FC<PdfViewerPageProps> = ({
   domID = "pdf-viewer-page",
@@ -22,6 +30,45 @@ const PdfViewerPage: FC<PdfViewerPageProps> = ({
     [dataTestId],
   );
 
+  const canvasRef = useRef<any>();
+  const [pdfRef, setPdfRef] = useState<any>();
+  // const [currentPage, setCurrentPage] = useState<number>(1);
+  const url = "test3.pdf";
+
+  pdfjs.GlobalWorkerOptions.workerSrc = require("pdfjs-dist/build/pdf.worker.entry");
+
+  const renderPage = useCallback(
+    (pageNum: number, pdf = pdfRef) => {
+      pdf &&
+        pdf.getPage(pageNum).then((page: any) => {
+          const viewport = page.getViewport({ scale: 1.5 });
+          const canvas = canvasRef.current;
+          const renderContext = {
+            canvasContext: canvas?.getContext("2d"),
+            viewport: viewport,
+          };
+          page.render(renderContext);
+          console.log(renderContext);
+        });
+    },
+    [pdfRef],
+  );
+  useEffect(() => {
+    renderPage(1, pdfRef);
+  }, [pdfRef, 1, renderPage]);
+
+  useEffect(() => {
+    const loadingTask = pdfjs.getDocument(url);
+    loadingTask.promise.then((loadedPdf: any) => {
+      setPdfRef(loadedPdf);
+    });
+  }, [url]);
+
+  // const nextPage = () =>
+  //   pdfRef && currentPage < pdfRef.numPages && setCurrentPage(currentPage + 1);
+
+  // const prevPage = () => currentPage > 1 && setCurrentPage(currentPage - 1);
+
   return (
     <div
       id={domIDs.root}
@@ -29,6 +76,9 @@ const PdfViewerPage: FC<PdfViewerPageProps> = ({
       data-testid={dataTestIDs.root}
     >
       <div>Pdf Viewer Page</div>
+      <div>
+        <canvas ref={canvasRef}></canvas>
+      </div>
     </div>
   );
 };
