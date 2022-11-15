ef364347b5833a0dab606325383b235dc82c0c39
nMedic246 <nikamedic14@gmail.com>
2022-11-15 10:18:44 +0100

    small changes

--- public/web/customToolbar.js
+++ public/web/customToolbar.js
@@ -1,23 +1,17 @@
 function editToolBar(){
-    //when the page is resized, the viewer hides and move some buttons around.
-    //this function forcibly show all buttons so none of them disappear or re-appear on page resize
-    
-
-    /* Changing icons */
-    
-    addElemFromSecondaryToPrimary("previous","toolbarViewerMiddle")
+    /* Moving elements*/    
+    //addElemFromSecondaryToPrimary("previous","toolbarViewerMiddle")
     /* Hiding elements */
-    removeElement("secondaryToolbarToggle")
-    removeElement("editorInk")
-    removeElement("editorFreeText")
-  
+    //removeElement("secondaryToolbarToggle")
+    removeElement("editorInk");
+    removeElement("editorFreeText");
 }
 
 function addElemFromSecondaryToPrimary(elemID, parentID){
     let element = document.getElementById(elemID);
     let parent = document.getElementById(parentID);
     element.style.minWidth = "0px";
-    element.innerHTML =""
+    element.innerHTML ="";
     parent.append(element);
 }
 function removeElement(elemID){
@@ -25,4 +19,4 @@ function removeElement(elemID){
     element.parentNode.removeChild(element);
 }
 
-window.onload = editToolBar
+window.onload = editToolBar;
