f29f4adba12aae51a2cd3d68dd5461f0d4358ea1
vlahovivan <ivan.vlahov@gmail.com>
2022-11-23 16:45:45 +0100

    enabled switching between tools

--- public/web/customToolbar.js
+++ public/web/customToolbar.js
@@ -1,22 +1,38 @@
-function editToolBar(){
-    /* Moving elements*/    
-    //addElemFromSecondaryToPrimary("previous","toolbarViewerMiddle")
-    /* Hiding elements */
-    //removeElement("secondaryToolbarToggle")
-    removeElement("editorInk");
-    removeElement("editorFreeText");
+function registerCustomToolbarButtonHandlers() {
+  const buttons = ["referenceDetails", "knowledgeGraph", "summary"];
+
+  buttons.forEach((btn) => {
+    document.getElementById(btn).onclick = () => {
+      const sidebarContent = document.getElementById("sidebarContent");
+      for (const child of sidebarContent.children) {
+        child.classList.add("hidden");
+      }
+
+      document.getElementById(`${btn}View`)?.classList.remove("hidden");
+    };
+  });
 }
 
-function addElemFromSecondaryToPrimary(elemID, parentID){
-    let element = document.getElementById(elemID);
-    let parent = document.getElementById(parentID);
-    element.style.minWidth = "0px";
-    element.innerHTML ="";
-    parent.append(element);
+function editToolBar() {
+  /* Moving elements*/
+  //addElemFromSecondaryToPrimary("previous","toolbarViewerMiddle")
+  /* Hiding elements */
+  //removeElement("secondaryToolbarToggle")
+  // removeElement("editorInk");
+  // removeElement("editorFreeText");
+  registerCustomToolbarButtonHandlers();
 }
-function removeElement(elemID){
-    let element = document.getElementById(elemID);
-    element.parentNode.removeChild(element);
+
+function addElemFromSecondaryToPrimary(elemID, parentID) {
+  let element = document.getElementById(elemID);
+  let parent = document.getElementById(parentID);
+  element.style.minWidth = "0px";
+  element.innerHTML = "";
+  parent.append(element);
+}
+function removeElement(elemID) {
+  let element = document.getElementById(elemID);
+  element.parentNode.removeChild(element);
 }
 
 window.onload = editToolBar;
