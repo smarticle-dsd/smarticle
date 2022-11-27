053e4a40f2047ca71ad14c3a30f5c67440387f8a
darioN556 <86221530+darioN556@users.noreply.github.com>
2022-11-27 11:02:26 +0100

    Refactored to simplify logic in FE

--- amplify/backend/function/getKnowledgeGraph/src/index.js
+++ amplify/backend/function/getKnowledgeGraph/src/index.js
@@ -2,6 +2,7 @@
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 exports.handler = async (event) => {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
   const axios = require("axios");
   return await axios
     .get(`https://api.semanticscholar.org/graph/v1/paper/` + event.paperId, {
@@ -23,43 +24,40 @@ exports.handler = async (event) => {
       // Number of vertices in the adjacency matrix, 1 is added for current paper
       let verticesCount = referenceCount + citationCount + 1;
 
-      // Creates N*N matrix with initial values set to 0
-      let nodesAndEdges = new Array(verticesCount);
+      let nodesAndEdges = [];
 
-      nodesAndEdges[0] = {
-        nodeData: {
-          data: { id: paperInfo.data.paperId, label: paperInfo.data.title },
-        },
-      };
+      nodesAndEdges.push({
+        data: { id: paperInfo.data.paperId, label: paperInfo.data.title },
+      });
 
       // Sets up the directed connections from current paper to all papers it references
       for (i = 1; i < referenceCount + 1; i++) {
         let reference = paperInfo.data.references[i - 1];
-        nodesAndEdges[i] = {
-          nodeData: { data: { id: reference.paperId, label: reference.title } },
-          edgeData: {
-            data: {
-              source: paperInfo.data.paperId,
-              target: reference.paperId,
-              label: "Edge label placeholder",
-            },
+        nodesAndEdges.push({
+          data: { id: reference.paperId, label: reference.title },
+        });
+        nodesAndEdges.push({
+          data: {
+            source: paperInfo.data.paperId,
+            target: reference.paperId,
+            label: "Edge label placeholder",
           },
-        };
+        });
       }
 
       // Sets up the directed connections from papers which cite this paper to the current paper
       for (i = referenceCount + 1; i < verticesCount; i++) {
-        let reference = paperInfo.data.citations[i - referenceCount - 1];
-        nodesAndEdges[i] = {
-          nodeData: { data: { id: reference.paperId, label: reference.title } },
-          edgeData: {
-            data: {
-              source: paperInfo.data.paperId,
-              target: reference.paperId,
-              label: "Edge label placeholder",
-            },
+        let citation = paperInfo.data.citations[i - referenceCount - 1];
+        nodesAndEdges.push({
+          data: { id: citation.paperId, label: citation.title },
+        });
+        nodesAndEdges.push({
+          data: {
+            source: paperInfo.data.paperId,
+            target: citation.paperId,
+            label: "Edge label placeholder",
           },
-        };
+        });
       }
 
       return {
@@ -67,8 +65,7 @@ exports.handler = async (event) => {
         body: nodesAndEdges,
       };
     })
-    .catch(function (error) {
-      console.log(error);
+    .catch(function () {
       return {
         statusCode: 400,
         body: "Failed when searching for paper with ID: " + event.paperId,
