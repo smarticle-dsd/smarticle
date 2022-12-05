fcb82a7260371b9597024bcc11e95ab06668216a
Sanjay <msanjaysivakumar@gmail.com>
2022-12-05 14:12:08 +0100

    update amplify

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,32 +1,32 @@
 export type AmplifyDependentResourcesAttributes = {
-    "function": {
-        "getPaperInfo": {
-            "Name": "string",
-            "Arn": "string",
-            "Region": "string",
-            "LambdaExecutionRole": "string"
-        },
-        "getKnowledgeGraph": {
-            "Name": "string",
-            "Arn": "string",
-            "Region": "string",
-            "LambdaExecutionRole": "string"
-        },
-        "getSummaryInfo": {
-            "Name": "string",
-            "Arn": "string",
-            "Region": "string",
-            "LambdaExecutionRole": "string"
-        },
-        "smarticlelayer": {
-            "Arn": "string"
-        }
-    },
-    "api": {
-        "backend": {
-            "RootUrl": "string",
-            "ApiName": "string",
-            "ApiId": "string"
-        }
-    }
-}
+  function: {
+    getPaperInfo: {
+      Name: "string";
+      Arn: "string";
+      Region: "string";
+      LambdaExecutionRole: "string";
+    };
+    getKnowledgeGraph: {
+      Name: "string";
+      Arn: "string";
+      Region: "string";
+      LambdaExecutionRole: "string";
+    };
+    getSummaryInfo: {
+      Name: "string";
+      Arn: "string";
+      Region: "string";
+      LambdaExecutionRole: "string";
+    };
+    smarticlelayer: {
+      Arn: "string";
+    };
+  };
+  api: {
+    backend: {
+      RootUrl: "string";
+      ApiName: "string";
+      ApiId: "string";
+    };
+  };
+};
