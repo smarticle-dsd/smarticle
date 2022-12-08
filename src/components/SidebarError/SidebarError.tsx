cf7102bc963324cfcba2bcb530daaef16c2123e7
sairaghav <4383992+sairaghav@users.noreply.github.com>
2022-12-08 18:49:36 +0100

    Common sidebar error component

---
+++ src/components/SidebarError/SidebarError.tsx
@@ -0,0 +1,61 @@
+import React, { useMemo, FC } from "react";
+import cs from "classnames";
+
+import { SidebarErrorProps } from "./SidebarError.types";
+import { Button } from "../Button";
+
+const SidebarError: FC<SidebarErrorProps> = ({
+  domID = "sidebar-error",
+  dataTestId = "test-sidebar-error",
+  className,
+}): JSX.Element => {
+  const domIDs = useMemo(
+    () => ({
+      root: domID,
+    }),
+    [domID],
+  );
+
+  const dataTestIDs = useMemo(
+    () => ({
+      root: dataTestId,
+    }),
+    [dataTestId],
+  );
+
+  return (
+    <div
+      id={domIDs.root}
+      className={cs("sa-sidebar-error", className)}
+      data-testid={dataTestIDs.root}
+    >
+      <div className={cs("sidebar-error-view-wrapper", className)}>
+        <div className={cs("sidebar-error-view", className)}>
+          <img src={"/sidebar-error.svg"} alt="Paper not found" />
+          <h3>Paper ID not found!</h3>
+        </div>
+      </div>
+      <div className={cs("sidebar-error-paper-input")}>
+        <div className={cs("sidebar-error-input-header", className)}>
+          <h3>Enter the paper's ID manually:</h3>
+        </div>
+        <div className={cs("sidebar-error-input-section", className)}>
+          <input
+            className={cs("sidebar-error-input-text", className)}
+            type="text"
+            placeholder="Enter ID"
+            onInput={() => {}}
+            onChange={(event) => {}}
+          />
+        </div>
+        <div className={cs("sidebar-error-button", className)}>
+          <Button size="large" type="primary" onClick={() => {}}>
+            Submit ID
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SidebarError;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 /* PLOP_INJECT_IMPORT */
+export * from "./SidebarError";
 export * from "./Reference";
 export * from "./Summary";
 export * from "./TestTool";
