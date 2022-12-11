3c6e8d3cac3e183fc85fb3e3bf6f699097e92e12
sairaghav <4383992+sairaghav@users.noreply.github.com>
2022-12-12 00:16:01 +0100

    Init tests

--- src/components/Header/__test__/Header.test.tsx
+++ src/components/Header/__test__/Header.test.tsx
@@ -1,23 +1,37 @@
 import * as React from "react";
 import { render } from "@testing-library/react";
 
+import { BrowserRouter } from "react-router-dom";
+
 import Header from "../Header";
 
 describe("Header tests", () => {
   test("Check if className exists on root DOM element", () => {
-    const { container } = render(<Header />);
+    const { container } = render(
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>,
+    );
     const element = container.querySelector(".sa-header");
     expect(element).not.toBe(null);
   });
 
   test("Check if domID exists on root DOM element", () => {
-    const { container } = render(<Header domID="header" />);
+    const { container } = render(
+      <BrowserRouter>
+        <Header domID="header" />
+      </BrowserRouter>,
+    );
     const element = container.querySelector("#header");
     expect(element).not.toBe(null);
   });
 
   test("Check if dataTestId exists on root DOM element", () => {
-    const { container } = render(<Header dataTestId="test-header" />);
+    const { container } = render(
+      <BrowserRouter>
+        <Header dataTestId="test-header" />
+      </BrowserRouter>,
+    );
     const element = container.querySelector('[data-testid="test-header"]');
     expect(element).not.toBe(null);
   });
--- src/pages/PdfViewerPage/__test__/PdfViewerPage.test.tsx
+++ src/pages/PdfViewerPage/__test__/PdfViewerPage.test.tsx
@@ -1,24 +1,36 @@
 import * as React from "react";
 import { render } from "@testing-library/react";
 
+import { BrowserRouter } from "react-router-dom";
+
 import PdfViewerPage from "../PdfViewerPage";
 
 describe("PdfViewerPage tests", () => {
   test("Check if className exists on root DOM element", () => {
-    const { container } = render(<PdfViewerPage />);
+    const { container } = render(
+      <BrowserRouter>
+        <PdfViewerPage />
+      </BrowserRouter>,
+    );
     const element = container.querySelector(".sa-pdf-viewer-page");
     expect(element).not.toBe(null);
   });
 
   test("Check if domID exists on root DOM element", () => {
-    const { container } = render(<PdfViewerPage domID="pdf-viewer-page" />);
+    const { container } = render(
+      <BrowserRouter>
+        <PdfViewerPage domID="pdf-viewer-page" />
+      </BrowserRouter>,
+    );
     const element = container.querySelector("#pdf-viewer-page");
     expect(element).not.toBe(null);
   });
 
   test("Check if dataTestId exists on root DOM element", () => {
     const { container } = render(
-      <PdfViewerPage dataTestId="test-pdf-viewer-page" />,
+      <BrowserRouter>
+        <PdfViewerPage dataTestId="test-pdf-viewer-page" />
+      </BrowserRouter>,
     );
     const element = container.querySelector(
       '[data-testid="test-pdf-viewer-page"]',
