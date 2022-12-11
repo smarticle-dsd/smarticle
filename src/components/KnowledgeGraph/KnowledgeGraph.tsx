62fb38e88a8325a3044db0b1fcecc3b65263e0fc
darioN556 <86221530+darioN556@users.noreply.github.com>
2022-12-11 14:58:39 +0100

    Implemented dynamic width based on parent container and error handling

--- src/components/KnowledgeGraph/KnowledgeGraph.tsx
+++ src/components/KnowledgeGraph/KnowledgeGraph.tsx
@@ -1,4 +1,5 @@
-import React, { useMemo, FC } from "react";
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { useMemo, FC, useState } from "react";
 import cs from "classnames";
 import CytoscapeComponent from "react-cytoscapejs";
 
@@ -23,59 +24,106 @@ const KnowledgeGraph: FC<KnowledgeGraphProps> = ({
     [dataTestId],
   );
 
+  const [error, setError] = useState<boolean>(false);
+
+  // create a reference to the parent container element
+  const containerRef = React.useRef<HTMLDivElement>(null);
+
+  // use the useState hook to store the width and height of the component
+  const [style, setStyle] = React.useState({});
+
+  // update the width  of the component when the component is mounted
+  React.useEffect(() => {
+    if (containerRef.current) {
+      setStyle({ width: containerRef.current.offsetWidth, height: 500 });
+    }
+  }, []);
+
+  // update the width of the component whenever the parent container changes
+  React.useEffect(() => {
+    function handleResize() {
+      if (containerRef.current) {
+        setStyle({ width: containerRef.current.offsetWidth, height: 500 });
+      }
+    }
+
+    // add an event listener to listen for resize events
+    window.addEventListener("resize", handleResize);
+  });
+
   // eslint-disable-next-line import/no-webpack-loader-syntax, @typescript-eslint/no-var-requires
   const elements = require("./response.json");
 
   // eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-unused-vars
-  const style = require("./cy-style.json");
+  const graphStyle = require("./cy-style.json");
 
   const layout = {
     name: "concentric",
     startAngle: 0 * Math.PI,
     minNodeSpacing: 20,
     edgeLengthVal: 80,
+    /* boundingBox: {
+      x1: 100,
+      y1: 100,
+      w: 600 - 100,
+      h: 500,
+    }, */
     concentric: function (node: any) {
       return node.degree();
     },
     levelWidth: function (nodes: []) {
       return 2;
     },
+    fit: "true",
+    animate: "true",
   };
 
-  /* const stylesheet = [
-    {
-      selector: "node",
-      style: {
-        width: 20,
-        height: 20,
-        "background-color": (node: any) => getColorBasedOnType(node),
-      },
-    },
-    {
-      selector: "edge",
-      style: {
-        "line-color": (edge: any) => getColorBasedOnType(edge),
-      },
-    },
-  ]; */
+  graphStyle[1].style["background-color"] = (node: any) =>
+    getColorBasedOnType(node);
 
-  style[1].style["background-color"] = (node: any) => getColorBasedOnType(node);
-
-  //console.log(style);
+  const [selectedPaperTitle, selectNode] = useState();
 
   return (
     <div
       id={domIDs.root}
       className={cs("sa-knowledge-graph", className)}
       data-testid={dataTestIDs.root}
+      ref={containerRef}
     >
-      <CytoscapeComponent
-        elements={elements}
-        style={{ width: "500px", height: "500px" }}
-        stylesheet={style}
-        layout={layout}
-        wheelSensitivity={0.2}
-      />
+      <h1>Knowledge Graph</h1>
+      {error && (
+        <div>
+          <div>
+            <img src={"/knowledgeGraph-error.svg"} alt="Paper not found" />
+            <h3>Paper ID not found!</h3>
+          </div>
+        </div>
+      )}
+      {!error && (
+        <CytoscapeComponent
+          cy={(cy) => {
+            cy.on("select", "node", (event) => {
+              // get the selected node
+              const node = event.target;
+
+              // get the data associated with the node
+              const data = node.data();
+              setError(false);
+
+              // do something with the data here
+              selectNode(data.label);
+            });
+          }}
+          elements={elements}
+          style={style}
+          stylesheet={graphStyle}
+          layout={layout}
+          wheelSensitivity={0.2}
+          pan={{ x: 100, y: 200 }}
+          zoom={0.3}
+        />
+      )}
+      <span>{selectedPaperTitle}</span>
     </div>
   );
 };
@@ -83,11 +131,11 @@ const KnowledgeGraph: FC<KnowledgeGraphProps> = ({
 function getColorBasedOnType(obj: any) {
   switch (obj.data("type")) {
     case "main":
-      return "red";
+      return "#8B0000";
     case "citation":
-      return "blue";
+      return "#00BFFF";
     case "reference":
-      return "green";
+      return "#009933";
     default:
       return "black";
   }
