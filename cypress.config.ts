25e315177c868d582d25091271c035bf9efab262
sairaghav <4383992+sairaghav@users.noreply.github.com>
2022-12-16 15:32:47 +0100

    Added config for prod

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,9 +3,10 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl:
-      process.env.REACT_APP_ENV === "prod"
-        ? "https://main.dcfoxoxqw27pv.amplifyapp.com/"
-        : "http://localhost:3000",
+      // process.env.REACT_APP_ENV === "prod"
+      //   ? "https://main.dcfoxoxqw27pv.amplifyapp.com/"
+      //   :
+      "http://localhost:3000",
     video: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
