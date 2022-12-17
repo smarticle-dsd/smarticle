4dde0f0d21031cd6976cdf05bc4e5440c220dd0f
sairaghav <4383992+sairaghav@users.noreply.github.com>
2022-12-17 11:47:26 +0100

    Removed unnecessary comment

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,11 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl:
-      // process.env.REACT_APP_ENV === "prod"
-      //   ? "https://main.dcfoxoxqw27pv.amplifyapp.com/"
-      //   :
-      "http://localhost:3000",
+    baseUrl: "http://localhost:3000",
     video: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
