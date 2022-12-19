67faf6e4e37526aca000425ded5deb1dfd6f195a
sairaghav <4383992+sairaghav@users.noreply.github.com>
2022-12-19 12:09:26 +0100

    Added tests for summary

--- cypress/e2e/summary.cy.ts
+++ cypress/e2e/summary.cy.ts
@@ -1,8 +1,10 @@
-const pdfUrlWithSummaryAbstract = "https://arxiv.org/pdf/2211.13228.pdf";
-const pdfUrlWithoutAbstract = "https://arxiv.org/pdf/2006.16630.pdf";
+const pdfUrlWithSummaryAbstract = "https://arxiv.org/pdf/2211.14227.pdf";
+const pdfUrlWithoutSummary = "https://arxiv.org/pdf/1305.3823.pdf";
 const pdfUrlWithNoDetails = "https://arxiv.org/pdf/2211.14250.pdf";
+const paperIdWithSummaryAbstract = "649def34f8be52c8b66281af98ae884c09aef38b";
+const paperIdWithoutSummary = "arxiv:1305.3823";
 const pdfFile = "cypress/fixtures/test1.pdf";
-const getIframeFromUrl = (pdfUrl) => {
+const getIframeFromUrl = (pdfUrl: string) => {
   cy.visit("/");
   return cy
     .get(".upload-pdf-button")
@@ -11,163 +13,269 @@ const getIframeFromUrl = (pdfUrl) => {
     .type(pdfUrl)
     .get(".modal-upload-button")
     .click()
-    .wait(2000)
+    .wait(10000)
     .get("iframe")
     .its("0.contentDocument.body")
     .should("not.be.empty")
     .then(cy.wrap);
 };
 
-// describe("The summary component opens when clicking on the Summary button on the toolbar", () => {
-//   it("passes", () => {
-//     getIframeFromUrl(pdfUrlWithSummaryAbstract).find("#summary").click();
-//   });
-// });
+describe("The summary component opens when clicking on the Summary button on the toolbar", () => {
+  it("passes", () => {
+    getIframeFromUrl(pdfUrlWithSummaryAbstract).find("#summary").click();
+  });
+});
 
-// describe("The summary and abstract are displayed when Semantic Scholar has summary and abstract for provided paper", () => {
-//   it("passes", () => {
-//     getIframeFromUrl(pdfUrlWithSummaryAbstract)
-//       .find(".sa-summary")
-//       .should(($summary) => {
-//         const tldr = $summary.find(".sa-summary-tldr");
-//         expect(tldr).to.contain.text("Summary");
-//         expect(tldr).to.contain.text(
-//           "A new perspective of self-supervised learning based on extending heat equation into high dimensional feature space is presented and an insightful hypothesis on the invariance within visual representation over different shapes and textures: the linear relationship between horizontal and vertical derivatives is provided.",
-//         );
-//         const abstract = $summary.find(".sa-summary-abstract");
-//         expect(abstract).to.contain.text("Abstract");
-//         expect(abstract).to.contain.text(
-//           "This paper presents a new perspective of self-supervised learning based on extending heat equation into high dimensional feature space. In particular, we remove time depen-dence by steady-state condition, and extend the remaining 2D Laplacian from x – y isotropic to linear correlated.",
-//         );
-//       });
-//   });
-// });
+describe("The summary and abstract are displayed when Semantic Scholar has summary and abstract for provided paper", () => {
+  it("passes", () => {
+    getIframeFromUrl(pdfUrlWithSummaryAbstract)
+      .find(".sa-summary")
+      .then(($summary) => {
+        const tldr = $summary.find(".sa-summary-tldr");
+        expect(tldr).to.contain.text("Summary");
+        expect(tldr).to.contain.text(
+          "A new preprocessing method is presented that simply stores the weight-data correlation in a tree data structure in order to quickly, dynamically detect which neurons are detected at each iteration, and it is proved that assuming a popular conjecture from complexity theory, one could not substantially speed up the algorithm for dynamic detection ofring neurons.",
+        );
+        const abstract = $summary.find(".sa-summary-abstract");
+        expect(abstract).to.contain.text("Abstract");
+        expect(abstract).to.contain.text(
+          "Over the last decade, deep neural networks have transformed our society, and they are already widely applied in various machine learning applications",
+        );
+      });
+  });
+});
 
-// describe("The abstract is displayed when Semantic Scholar has summary but no abstract for provided paper", () => {
-//   it("passes", () => {
-//     getIframeFromUrl(pdfUrlWithoutAbstract)
-//       .find(".sa-summary")
-//       .should(($summary) => {
-//         const tldr = $summary.find(".sa-summary-tldr");
-//         expect(tldr).to.contain.text("Summary");
-//         expect(tldr).to.contain.text(
-//           "This book chapter shows how VC theory can be derived from a detailed electrodiffusive theory for ion concentration dynamics in the extracellular medium, and it shows what assumptions must be introduced to get the VC theory on the simplified form that is commonly used by neuroscientists.",
-//         );
-//         const abstract = $summary.find(".sa-summary-abstract");
-//         expect(abstract).not.to.contain.text("Abstract");
-//         expect(abstract).to.be.empty;
-//       });
-//   });
-// });
+describe("The available data is displayed when Semantic Scholar has either summary or abstract for provided paper", () => {
+  it("passes", () => {
+    getIframeFromUrl(pdfUrlWithoutSummary)
+      .find(".sa-summary")
+      .then(($summary) => {
+        const tldr = $summary.find(".sa-summary-tldr");
+        expect(tldr.text()).to.equal("Summary");
+        const abstract = $summary.find(".sa-summary-abstract");
+        expect(abstract).to.contain.text("Abstract");
+        expect(abstract).to.contain.text(
+          "In this paper we study the azimuthal correlations of heavy quarks in Pb+Pb collisions with",
+        );
+      });
+  });
+});
 
-// describe("The summary is displayed when Semantic Scholar has abstract but no summary for provided paper", () => {
-//   it("passes", () => {
-//     getIframeFromUrl(pdfUrlWithoutAbstract)
-//       .find(".sa-summary")
-//       .should(($summary) => {
-//         const tldr = $summary.find(".sa-summary-tldr");
-//         expect(tldr).to.contain.text("Summary");
-//         expect(tldr).to.contain.text(
-//           "This book chapter shows how VC theory can be derived from a detailed electrodiffusive theory for ion concentration dynamics in the extracellular medium, and it shows what assumptions must be introduced to get the VC theory on the simplified form that is commonly used by neuroscientists.",
-//         );
-//         const abstract = $summary.find(".sa-summary-abstract");
-//         expect(abstract).not.to.contain.text("Abstract");
-//         expect(abstract).to.be.empty;
-//       });
-//   });
-// });
-
-// describe("Error message is displayed when Semantic Scholar does not have summary and abstract for provided paper", () => {
-//   it("passes", () => {
-//     getIframeFromUrl(pdfUrlWithNoDetails)
-//       .find(".sa-summary")
-//       .should(($summary) => {
-//         const tldr = $summary.find(".sa-summary-tldr");
-//         expect(tldr).to.contain.text("Summary");
-//         expect(tldr.text()).to.equal("Summary");
-//         const abstract = $summary.find(".sa-summary-abstract");
-//         expect(abstract).not.to.contain.text("Abstract");
-//         expect(abstract).to.be.empty;
-//         const error = $summary.find(".sa-summary-error");
-//         expect(error).to.exist;
-//         expect(error.text()).to.contain("Paper ID not found!");
-//         expect(error.text()).to.contain("Supported ID formats and examples:");
-//       });
-//   });
-// });
-
-// describe('"Submit ID" and "Reset" buttons are disabled when paper is loaded without any user input', () => {
-//   it("passes", () => {
-//     getIframeFromUrl(pdfUrlWithNoDetails)
-//       .find(".sa-summary")
-//       .should(($summary) => {
-//         const submitButton = $summary.find(".sidebar-submit-button");
-//         expect(submitButton).to.be.disabled;
-//         const resetButton = $summary.find(".sidebar-reset-button");
-//         expect(resetButton).to.be.disabled;
-//       });
-//   });
-// });
-
-describe('"Submit ID" button is enabled when user starts typing input in the input field', () => {
+describe("Error message is displayed when Semantic Scholar does not have summary and abstract for provided paper", () => {
   it("passes", () => {
     getIframeFromUrl(pdfUrlWithNoDetails)
       .find(".sa-summary")
-      .should(($summary) => {
-        const inputText = $summary.find(".sidebar-error-input-text");
-        inputText.type("abcd");
+      .then(($summary) => {
+        const tldr = $summary.find(".sa-summary-tldr");
+        expect(tldr).to.contain.text("Summary");
+        expect(tldr.text()).to.equal("Summary");
+        const abstract = $summary.find(".sa-summary-abstract");
+        expect(abstract).to.be.empty;
+        const error = $summary.find(".sa-summary-error");
+        expect(error).to.exist;
+        expect(error.text()).to.contain("Paper ID not found!");
+        expect(error.text()).to.contain("Supported ID formats and examples:");
+      });
+  });
+});
+
+describe('"Submit ID" and "Reset" buttons are disabled when paper is loaded without any user input', () => {
+  it("passes", () => {
+    getIframeFromUrl(pdfUrlWithNoDetails)
+      .find(".sa-summary")
+      .then(($summary) => {
         const submitButton = $summary.find(".sidebar-submit-button");
-        expect(submitButton).to.not.be.disabled;
+        expect(submitButton).to.be.disabled;
         const resetButton = $summary.find(".sidebar-reset-button");
         expect(resetButton).to.be.disabled;
       });
   });
 });
 
-// describe('"Reset" button is enabled when user has generated summary for custom paper ID', () => {
-//   it("passes", () => {
-//     getIframeFromUrl("https://arxiv.org/pdf/2211.13228.pdf")
-//       .find("#summary")
-//       .click();
-//   });
-// });
+describe('"Submit ID" button is enabled when user starts typing input in the input field', () => {
+  it("passes", () => {
+    getIframeFromUrl(pdfUrlWithNoDetails)
+      .find(".sa-summary")
+      .then(($summary) => {
+        const inputText = $summary.find(".sidebar-error-input-text");
+        const submitButton = $summary.find(".sidebar-submit-button");
+        expect(submitButton).to.be.disabled;
+        const resetButton = $summary.find(".sidebar-reset-button");
+        expect(resetButton).to.be.disabled;
+        cy.wrap(inputText)
+          .type("abcd", { force: true })
+          .then(() => {
+            expect(submitButton).to.not.be.disabled;
+            expect(resetButton).to.be.disabled;
+          });
+      });
+  });
+});
 
-// describe('Clicking on "Reset" button resets the summary to the originally generated summary/error', () => {
-//   it("passes", () => {
-//     getIframeFromUrl("https://arxiv.org/pdf/2211.13228.pdf")
-//       .find("#summary")
-//       .click();
-//   });
-// });
+describe('"Reset" button is enabled when user has generated summary for custom paper ID', () => {
+  it("passes", () => {
+    getIframeFromUrl(pdfUrlWithSummaryAbstract)
+      .find(".sa-summary")
+      .then(($summary) => {
+        const inputText = $summary.find(".sidebar-error-input-text");
+        const submitButton = $summary.find(".sidebar-submit-button");
+        expect(submitButton).to.be.disabled;
+        const resetButton = $summary.find(".sidebar-reset-button");
+        expect(resetButton).to.be.disabled;
 
-// describe('Entering a paper ID and clicking on "Submit ID" with abstract and summary displays both summary and abstract', () => {
-//   it("passes", () => {
-//     getIframeFromUrl("https://arxiv.org/pdf/2211.13228.pdf")
-//       .find("#summary")
-//       .click();
-//   });
-// });
+        cy.wrap(inputText)
+          .type(paperIdWithSummaryAbstract, { force: true })
+          .then(() => {
+            expect(submitButton).to.not.be.disabled;
+            expect(resetButton).to.be.disabled;
+            cy.wrap(submitButton)
+              .click({ force: true })
+              .wait(5000)
+              .then(() => {
+                expect(resetButton).to.not.be.disabled;
+              });
+          });
+      });
+  });
+});
 
-// describe('Entering a paper ID and clicking on "Submit ID" with only abstract displays only abstract', () => {
-//   it("passes", () => {
-//     getIframeFromUrl("https://arxiv.org/pdf/2211.13228.pdf")
-//       .find("#summary")
-//       .click();
-//   });
-// });
+describe('Clicking on "Reset" button resets the summary to the originally generated summary/error', () => {
+  it("passes", () => {
+    getIframeFromUrl(pdfUrlWithSummaryAbstract)
+      .find(".sa-summary")
+      .then(($summary) => {
+        const inputText = $summary.find(".sidebar-error-input-text");
+        const submitButton = $summary.find(".sidebar-submit-button");
+        expect(submitButton).to.be.disabled;
+        const resetButton = $summary.find(".sidebar-reset-button");
+        expect(resetButton).to.be.disabled;
+        const tldr = $summary.find(".sa-summary-tldr");
+        const abstract = $summary.find(".sa-summary-abstract");
 
-// describe('Entering a paper ID and clicking on "Submit ID" with only summary displays only summary', () => {
-//   it("passes", () => {
-//     getIframeFromUrl("https://arxiv.org/pdf/2211.13228.pdf")
-//       .find("#summary")
-//       .click();
-//   });
-// });
+        cy.wrap(inputText)
+          .type(paperIdWithSummaryAbstract, { force: true })
+          .then(() => {
+            expect(submitButton).to.not.be.disabled;
+            expect(resetButton).to.be.disabled;
+            cy.wrap(submitButton)
+              .click({ force: true })
+              .wait(5000)
+              .then(() => {
+                cy.wrap(resetButton)
+                  .click({ force: true })
+                  .wait(5000)
+                  .then(() => {
+                    expect(tldr).to.contain.text(
+                      "A new preprocessing method is presented that simply stores the weight-data correlation in a tree data structure in order to quickly, dynamically detect which neurons are detected at each iteration, and it is proved that assuming a popular conjecture from complexity theory, one could not substantially speed up the algorithm for dynamic detection ofring neurons.",
+                    );
+                    expect(abstract).to.contain.text(
+                      "Over the last decade, deep neural networks have transformed our society, and they are already widely applied in various machine learning applications",
+                    );
+                  });
+              });
+          });
+      });
+  });
+});
 
-// describe('Entering an invalid paper ID and clicking on "Submit ID" displays error message', () => {
-//   it("passes", () => {
-//     getIframeFromUrl("https://arxiv.org/pdf/2211.13228.pdf")
-//       .find("#summary")
-//       .click();
-//   });
-// });
+describe('Entering a paper ID with abstract and summary and clicking on "Submit ID" displays both summary and abstract', () => {
+  it("passes", () => {
+    getIframeFromUrl(pdfUrlWithSummaryAbstract)
+      .find(".sa-summary")
+      .then(($summary) => {
+        const inputText = $summary.find(".sidebar-error-input-text");
+        const submitButton = $summary.find(".sidebar-submit-button");
+        expect(submitButton).to.be.disabled;
+        const resetButton = $summary.find(".sidebar-reset-button");
+        expect(resetButton).to.be.disabled;
+        const tldr = $summary.find(".sa-summary-tldr");
+        const abstract = $summary.find(".sa-summary-abstract");
+
+        cy.wrap(inputText)
+          .type(paperIdWithSummaryAbstract, { force: true })
+          .then(() => {
+            expect(submitButton).to.not.be.disabled;
+            expect(resetButton).to.be.disabled;
+            cy.wrap(submitButton)
+              .click({ force: true })
+              .wait(5000)
+              .then(() => {
+                expect(tldr).to.contain.text(
+                  "This paper reduces literature graph construction into familiar NLP tasks, point out research challenges due to differences from standard formulations of these tasks, and report empirical results for each task.",
+                );
+                expect(abstract).to.contain.text(
+                  "We describe a deployed scalable system for organizing published scientific literature into a heterogeneous graph to facilitate algorithmic manipulation and discovery.",
+                );
+              });
+          });
+      });
+  });
+});
+
+describe('Entering a paper ID with either abstract or summary and clicking on "Submit ID" displays only available data', () => {
+  it("passes", () => {
+    getIframeFromUrl(pdfUrlWithoutSummary)
+      .find(".sa-summary")
+      .then(($summary) => {
+        const inputText = $summary.find(".sidebar-error-input-text");
+        const submitButton = $summary.find(".sidebar-submit-button");
+        expect(submitButton).to.be.disabled;
+        const resetButton = $summary.find(".sidebar-reset-button");
+        expect(resetButton).to.be.disabled;
+        const tldr = $summary.find(".sa-summary-tldr");
+        const abstract = $summary.find(".sa-summary-abstract");
+
+        cy.wrap(inputText)
+          .type(paperIdWithoutSummary, { force: true })
+          .then(() => {
+            expect(submitButton).to.not.be.disabled;
+            expect(resetButton).to.be.disabled;
+            cy.wrap(submitButton)
+              .click({ force: true })
+              .wait(5000)
+              .then(() => {
+                expect(abstract).to.contain.text(
+                  "In this paper we study the azimuthal correlations of heavy quarks in Pb+Pb collisions with",
+                );
+                expect(tldr.text()).to.equal("Summary");
+              });
+          });
+      });
+  });
+});
+
+describe('Entering an invalid paper ID and clicking on "Submit ID" displays error message', () => {
+  it("passes", () => {
+    getIframeFromUrl(pdfUrlWithNoDetails)
+      .find(".sa-summary")
+      .then(($summary) => {
+        const inputText = $summary.find(".sidebar-error-input-text");
+        const submitButton = $summary.find(".sidebar-submit-button");
+        expect(submitButton).to.be.disabled;
+        const resetButton = $summary.find(".sidebar-reset-button");
+        expect(resetButton).to.be.disabled;
+        const tldr = $summary.find(".sa-summary-tldr");
+        const abstract = $summary.find(".sa-summary-abstract");
+        const error = $summary.find(".sa-summary-error");
+
+        cy.wrap(inputText)
+          .type("abcd", { force: true })
+          .then(() => {
+            expect(submitButton).to.not.be.disabled;
+            expect(resetButton).to.be.disabled;
+            cy.wrap(submitButton)
+              .click({ force: true })
+              .wait(5000)
+              .then(() => {
+                expect(tldr.text()).to.equal("Summary");
+                expect(abstract).to.be.empty;
+                expect(error).to.exist;
+                expect(error.text()).to.contain("Paper ID not found!");
+                expect(error.text()).to.contain(
+                  "Supported ID formats and examples:",
+                );
+              });
+          });
+      });
+  });
+});
