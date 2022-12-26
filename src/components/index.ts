25bc2a899dfd041c1ae967cc313aafc86da25815
Noemi Huang <80103978+noemi-i@users.noreply.github.com>
2022-12-26 17:01:17 +0100

    merge develop into branch

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,15 +1,12 @@
 /* PLOP_INJECT_IMPORT */
-<<<<<<< HEAD
-export * from "./FeatureTooltipHandler";
-export * from "./FeatureTooltip";
-=======
 export * from "./InputText";
 export * from "./KnowledgeGraphModal";
 export * from "./KnowledgeGraph";
 export * from "./TitleClose";
 export * from "./CustomSummary";
 export * from "./SidebarZoom";
->>>>>>> develop
+export * from "./FeatureTooltipHandler";
+export * from "./FeatureTooltip";
 export * from "./SupportedPaperList";
 export * from "./Loader";
 export * from "./SidebarError";
