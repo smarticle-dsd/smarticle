317a8bddf004f199e461bbe9ebeef4413228819f
nMedic246 <nikamedic14@gmail.com>
2022-12-26 14:35:04 +0100

    try1

--- src/components/Reference/Reference.tsx
+++ src/components/Reference/Reference.tsx
@@ -33,7 +33,7 @@ const Reference: FC<ReferenceProps> = ({
       <h1>Reference Details</h1>
 
       <div className={cs("reference-view-wrapper", className)}>
-        <div className={cs("reference-view", className)}>
+        <div className={cs('reference-view', className)}>
           <img src={svg} alt="Reference" />
           <h3>Click on a reference to preview it</h3>
         </div>
