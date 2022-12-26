e3f440e4d5d1a5da4c2176c313c6f8167f3e139e
Noemi Huang <80103978+noemi-i@users.noreply.github.com>
2022-12-26 16:34:35 +0100

    Open tooltips at user first visit

--- src/pages/PdfViewerPage/PdfViewerPage.tsx
+++ src/pages/PdfViewerPage/PdfViewerPage.tsx
@@ -4,8 +4,12 @@ import cs from "classnames";
 import { PdfViewerPageProps } from "./PdfViewerPage.types";
 import { useSearchParams } from "react-router-dom";
 import { createPortal } from "react-dom";
-import { TestTool, Summary } from "../../components";
-import { Reference } from "../../components";
+import {
+  Reference,
+  TestTool,
+  Summary,
+  FeatureTooltipHandler,
+} from "../../components";
 import * as pdfjs from "pdfjs-dist/legacy/build/pdf";
 import { TextItem, TextMarkedContent } from "pdfjs-dist/types/src/display/api";
 import { Error404Page } from "../Error404Page";
@@ -158,6 +162,23 @@ const PdfViewerPage: FC<PdfViewerPageProps> = ({
     }
   }, []);
 
+  const prefix = "/pdfviewer/";
+  const [isVisited, setIsVisited] = useState(false);
+  const [isTooltipVisible, setIsTooltipVisible] = useState(true);
+
+  const toogleTooltip = () => {
+    setIsTooltipVisible(!isTooltipVisible);
+  };
+
+  useEffect(() => {
+    const visited = localStorage.getItem("Visited");
+    if (window.location.href.includes(prefix) && !visited) {
+      console.log({ localStorage });
+      localStorage.setItem("Visited", "visited");
+      setIsVisited(true);
+    }
+  }, []);
+
   return (
     <div
       id={domIDs.root}
@@ -165,25 +186,33 @@ const PdfViewerPage: FC<PdfViewerPageProps> = ({
       data-testid={dataTestIDs.root}
     >
       {pdfFile.length > 0 ? (
-        <iframe
-          id="pdf-js-viewer"
-          src={pdfFile}
-          title="webviewer"
-          width="100%"
-          height="100%"
-          ref={viewerRef}
-        >
-          {referenceDetailsMountNode &&
-            createPortal(<Reference />, referenceDetailsMountNode)}
-          {knowledgeGraphMountNode &&
-            createPortal(<TestTool />, knowledgeGraphMountNode)}
-          {summaryMountNode &&
-            createPortal(
-              // Need to find a way to get this from the PDF Viewer component
-              <Summary paperTitle={paperTitle} />,
-              summaryMountNode,
-            )}
-        </iframe>
+        <>
+          <iframe
+            id="pdf-js-viewer"
+            src={pdfFile}
+            title="webviewer"
+            width="100%"
+            height="100%"
+            ref={viewerRef}
+          >
+            {referenceDetailsMountNode &&
+              createPortal(<Reference />, referenceDetailsMountNode)}
+            {knowledgeGraphMountNode &&
+              createPortal(<TestTool />, knowledgeGraphMountNode)}
+            {summaryMountNode &&
+              createPortal(
+                // Need to find a way to get this from the PDF Viewer component
+                <Summary paperTitle={paperTitle} />,
+                summaryMountNode,
+              )}
+          </iframe>
+          {isVisited ? (
+            <FeatureTooltipHandler
+              isVisible={isTooltipVisible}
+              close={toogleTooltip}
+            />
+          ) : null}
+        </>
       ) : (
         <Error404Page />
       )}
